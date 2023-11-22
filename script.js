341092ff5b640d7aafde4c2c080a3ea38b42d7f5
Robbianoremi <robbianoremi@gmail.com>
2023-11-22 13:45:00 +0100

    envoi du gitignore

---
+++ script.js
@@ -0,0 +1,11 @@
+// // Déclaration d'une fonction
+// function afficherMessage(hello) {
+//     // Création d'une variable contenant le message
+//     var message = "Bonjour, ceci est un exemple de code JavaScript !";
+  
+//     // Affichage du message dans la console
+//     console.log(message);
+//   }
+  
+//   // Appel de la fonction pour exécuter le code
+//   afficherMessage();
